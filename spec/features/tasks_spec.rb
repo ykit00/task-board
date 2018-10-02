bd7aeeb9cb98300dbf15cb7f58fae0326a13ab09
Yuki Ito <y.ito.works@gmail.com>
2018-10-02 15:46:42 +0900

    Add spec features tasks

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -13,4 +13,47 @@ RSpec.feature "Tasks", type: :feature do
     expect(page).to have_content "Test Task"
     expect(page).to have_content "Trying out Capybara"
   end
+
+  # タスクをの詳細を確認する
+  scenario "show a task" do
+    visit root_path
+    click_link "新規作成"
+    fill_in "Title", with: "Test Task"
+    fill_in "Description", with: "Trying out Capybara"
+    click_button "Create Task"
+
+    click_link "Test Task"
+    expect(page).to have_content "Test Task"
+    expect(page).to have_content "編集"
+    expect(page).to have_content "戻る"
+  end
+
+  # タスクを編集する
+  scenario "edit a task" do
+    visit root_path
+    click_link "新規作成"
+    fill_in "Title", with: "Test Task"
+    fill_in "Description", with: "Trying out Capybara"
+    click_button "Create Task"
+
+    click_link "編集"
+    fill_in "Description", with: "Change"
+    click_button "Update Task"
+
+    expect(page).to have_content "タスクを更新しました。"
+    expect(page).to have_content "Change"
+  end
+
+  # タスクを削除する
+  scenario "delete a task" do
+    visit root_path
+    click_link "新規作成"
+    fill_in "Title", with: "Test Task"
+    fill_in "Description", with: "Trying out Capybara"
+    click_button "Create Task"
+
+    click_link "削除"
+    expect(page).to have_content "タスクを削除しました。"
+    expect(page).to_not have_content "Test Task"
+  end
 end
