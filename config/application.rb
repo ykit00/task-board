a291a8bb0002ca6e24cdde794a56b42da0da7409
Yuki Ito <y.ito.works@gmail.com>
2018-10-02 14:12:58 +0900

    Add gem factory_bot_rails

--- config/application.rb
+++ config/application.rb
@@ -9,10 +9,10 @@ module TaskBoard
 
     config.generators do |g|
       g.test_framework :rspec,
-        fixtures: false,
         view_specs: false,
         helper_specs: false,
-        routing_specs: false
+        routing_specs: false,
+        request_specs: false
     end
   end
 end
