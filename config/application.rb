cb7053aac7dcc464adb88b69bd23162d016496fe
Yuki Ito <y.ito.works@gmail.com>
2018-10-03 18:45:44 +0900

    タイムゾーンを東京に設定

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,8 @@ module TaskBoard
   class Application < Rails::Application
     config.load_defaults 5.2
 
+    config.time_zone = 'Tokyo'
+
     config.generators do |g|
       g.test_framework :rspec,
         view_specs: false,
