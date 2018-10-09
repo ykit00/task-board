b646106c9d877e3750ce4c215e2f16d0b20beff6
Yuki Ito <y.ito.works@gmail.com>
2018-10-09 18:10:00 +0900

    スコープを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,8 @@ class Task < ApplicationRecord
   validates :description, length: { maximum: 100 }
   validate :deadline_is_not_past?
 
+  scope :deadline, -> { ofder(deadline: :asc) }
+
   def deadline_is_not_past?
     if deadline.present? && deadline.past?
       errors.add(:deadline, "は過去の日時を設定できません。")
