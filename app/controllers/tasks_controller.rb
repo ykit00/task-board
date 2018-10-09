02e7cd0060266fc99729504ff7f7b1f2c4e141b1
Yuki Ito <y.ito.works@gmail.com>
2018-10-09 15:21:50 +0900

    締め切りを追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -41,6 +41,6 @@ class TasksController < ApplicationController
     end
 
     def task_params
-      params.require(:task).permit(:title, :description)
+      params.require(:task).permit(:title, :description, :deadline)
     end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,11 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 30 }
   validates :description, length: { maximum: 100 }
+  validate :deadline_is_future?
+
+  def deadline_is_future?
+    if deadline.present? && deadline.past?
+      errors.add(:deadline, "は過去の日時を設定できません。")
+    end
+  end
 end
