c96f5378c5692dde35ef054e6002a7b5cd9766a4
Yuki Ito <y.ito.works@gmail.com>
2018-10-09 15:36:24 +0900

    メソッド名を変更

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,9 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 30 }
   validates :description, length: { maximum: 100 }
-  validate :deadline_is_future?
+  validate :deadline_is_not_past?
 
-  def deadline_is_future?
+  def deadline_is_not_past?
     if deadline.present? && deadline.past?
       errors.add(:deadline, "は過去の日時を設定できません。")
     end
