a1e85e29a07b21034b00be6db467773187cdc867
Yuki Ito <y.ito.works@gmail.com>
2018-10-10 16:12:43 +0900

    bootstrap4を導入

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,6 @@
 //= require activestorage
 //= require turbolinks
 //= require jquery/dist/jquery.js
+//= require popper.js/dist/popper.js
+//= require bootstrap/dist/js/bootstrap.js
 //= require_tree .
