f57a4c40e854a0e5d8367de813c4e4c883f05846
Yuki Ito <y.ito.works@gmail.com>
2018-10-15 01:03:24 +0900

    font-awesomeを追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,9 +4,12 @@ global.$ = require('jquery')
 // JS
 import 'popper.js/dist/popper';
 import 'bootstrap/dist/js/bootstrap';
+import '@fortawesome/fontawesome-free/js/all.js'
 
 // CSS
 import 'bootstrap/dist/css/bootstrap.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+
 
 import Rails from 'rails-ujs'
 
