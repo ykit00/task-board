5e2c24e8966d74a9f539a4538d3be632476f3165
Yuki Ito <y.ito.works@gmail.com>
2018-10-18 18:25:12 +0900

    クラスメソッドの位置を移動

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,18 +7,21 @@ class Task < ApplicationRecord
 
   enum status: { not_yet: 0, in_progress: 1, done: 2, pending: 3, discontinued: 4 }
 
-  private
-
-  def self.search(search)
-    if search
-      where ['title LIKE ?', "%#{search}%"]
-    else
-      all
+  class << self
+    def search(search)
+      if search
+        where 'title LIKE ?', "%#{search}%" # 一部一致で検索
+        # where 'title LIKE ?', search # 完全一致で検索
+      else
+        all
+      end
     end
   end
 
-  def deadline_is_not_past?
-    errors.add(:deadline, 'は過去の日時を設定できません。') if deadline.present? && deadline.past?
-  end
+  private
+
+    def deadline_is_not_past?
+      errors.add(:deadline, 'は過去の日時を設定できません。') if deadline.present? && deadline.past?
+    end
 
 end
