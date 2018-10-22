e540656f899fea90e5bb5d09eb5f92a8159f4a37
Yuki Ito <y.ito.works@gmail.com>
2018-10-22 10:00:20 +0900

    タスクのseedデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Tasks
+20.times do |n|
+  title = "Task#{n+1}"
+  status = [Task.statuses.to_a.sample].to_h.first[1]
+
+  Task.create!(title: title, status: status)
+end
