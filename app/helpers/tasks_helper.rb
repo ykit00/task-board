20e901e077428c4325d78ec6817e9c2fd8c27b3b
Yuki Ito <y.ito.works@gmail.com>
2018-11-28 16:38:48 +0900

    フィードバックを基にリファクタリング

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,20 +1,19 @@
 module TasksHelper
   def set_sort_button_attr(title, column)
-    if params[:sort_column].nil? && params[:sort_direction].nil?
-      direction = 'desc'
-      button = 'neutral'
-    elsif params[:sort_column] == column && params[:sort_direction] == 'desc'
-      direction = 'asc'
-      button = 'down'
-    elsif params[:sort_column] == column && params[:sort_direction] == 'asc'
-      column = nil
-      direction = nil
-      button = 'up'
-    else
-      direction = 'desc'
-      button = 'neutral'
+    direction = 'desc'
+    button = 'neutral'
+
+    if params[:sort_column] == column
+      if params[:sort_direction] == 'desc'
+        direction = 'asc'
+        button = 'down'
+      elsif params[:sort_direction] == 'asc'
+        column = nil
+        direction = nil
+        button = 'up'
+      end
     end
 
-    @sort_attr = { title: title, column: column, direction: direction, button: button }
+    { title: title, column: column, direction: direction, button: button }
   end
 end
